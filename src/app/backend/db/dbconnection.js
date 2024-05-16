b2c956603497f8a68c95fa5c9eaaef427f7d40c4
OsafTheDeveloper <osafh486@gmail.com>
2024-05-16 14:56:46 +0500

    commit

--- src/app/backend/db/dbconnection.js
+++ src/app/backend/db/dbconnection.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 const dbConnection = async () => {
   if (mongoose.connection.readyState >= 1) {
     console.log("mongodb is Already Connected!");
+    return;
   }
   try {
     await mongoose.connect("mongodb://127.0.0.1:27017/testing");
