38839ad26a92befdb57af7624f8434ea66b5fb10
ayeshairfan12 <irfanayesha712@gmail.com>
2024-05-17 18:55:50 +0500

    branches and branches admins endpoints

--- src/app/api/branch/route.js
+++ src/app/api/branch/route.js
@@ -1,15 +1,50 @@
 import { NextResponse } from 'next/server';
-import dbConnection from '@/app/backend/db/dbconnection';
-import { Branch } from '@/app/backend/models/branches';
-dbConnection();
+import dbConnection from '@/backend/db/dbconnection';
+import { Branch } from '@/backend/models/branches/branch';
+
 
 export async function POST(req) {
+  await dbConnection();
 
   try {
     const body = await req.json();
     const branch = await Branch.create(body)
-    return NextResponse.json({ success: true, data: branch }, { status: 201 });
+    return NextResponse.json({
+      message: "Branch Created!",
+      success: true,
+      data: branch,
+    });
+    } catch (error) {
+      console.log(error)
+      return NextResponse.json(
+        {
+          message: "Internal Server Error",
+          success: false,
+        },
+        { status: 201 }
+      );
+      }
+}
+
+export async function GET(req) {
+  await dbConnection();
+  try {
+    const branches = await Branch.find();
+    return NextResponse.json({
+      message: "All Branches Found",
+      success: true,
+      data: branches,
+    });
   } catch (error) {
-    return NextResponse.json({ success: false, error: error.message }, { status: 500 });
+    return NextResponse.json(
+      {
+        message: "Internal Server Error",
+        success: false,
+      },
+      { status: 500 }
+    );
   }
 }
+
+
+
