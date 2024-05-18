b10bb845f9675053c0905854720c005e5ac16502
mdanish73 <mdiqbal00037@gmail.com>
2024-05-18 14:20:14 +0500

    countries endpoints done

--- src/app/api/countries/[id]/route.js
+++ src/app/api/countries/[id]/route.js
@@ -23,3 +23,44 @@ export async function GET(req, {params}) {
     });
   }
 }
+
+export async function PUT (req, {params}) {
+  try {
+    const { id } = params;
+    const request = await req.json();
+    const updatedCountry = await countries.findByIdAndUpdate(id, request, { new: true, runValidators: true });
+    return NextResponse.json({
+      success: true,
+      message: updatedCountry
+    }, {
+      status: 200
+    });
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      message: error.message
+    }, {
+      status: 500
+    });
+  }
+}
+
+export async function DELETE (req, {params}) {
+  try {
+    const { id } = params;
+    const updatedCountry = await countries.findByIdAndDelete(id);
+    return NextResponse.json({
+      success: true,
+      message: "Country deleted successfully..."
+    }, {
+      status: 200
+    });
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      message: error.message
+    }, {
+      status: 500
+    });
+  }
+}
