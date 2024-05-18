dca911add0362a3101c253fa8f6892a8aa708009
OsafTheDeveloper <osafh486@gmail.com>
2024-05-18 13:51:33 +0500

    cty api

---
+++ src/app/api/city/route.js
@@ -0,0 +1,24 @@
+import dbConnection from "@/backend/db/dbconnection";
+import cities from "@/backend/models/cities/cities";
+import cityNames from "@/backend/models/cities/cityNames";
+import { NextResponse } from "next/server";
+
+dbConnection();
+const POST = async () => {
+  try {
+    const body = await req.json();
+    const createdCity = await cities.create(body);
+    return NextResponse.json({
+      message: "City Created",
+      success: true,
+      data: createdCity,
+    });
+  } catch (error) {
+    console.log(error.message);
+    return NextResponse.json({
+      message: "Internal Server Error",
+      success: false,
+    });
+  }
+};
+export { POST };
