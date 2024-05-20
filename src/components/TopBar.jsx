6db953d1aa96a942a046a87467c776b0a8eea625
codewithmati <m4mati10@gmail.com>
2024-05-20 18:43:57 +0500

    topbar

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,8 +1,11 @@
+import Image from 'next/image'
 import React from 'react'
 
 const TopBar = () => {
   return (
-    <div className='w-full bg-gradient-to-r from-[#121B2C] via-[#0E1726] to-[#0F1827] py-[10px] px-5 text-white'>TopBar</div>
+    <div className='w-full flex bg-[#0E1726] py-2.5 z-20 sticky px-5 text-white'>
+      <Image src={}/>
+    </div>
   )
 }
 
