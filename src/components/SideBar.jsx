fcf259dda122ab46d75b8eec636c47f1f7ea24b9
codewithmati <m4mati10@gmail.com>
2024-05-21 17:19:33 +0500

    topbar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,63 +1,53 @@
-'use client'
-import { ChevronRight, Home, Minus } from 'lucide-react';
-import React from 'react'
-import Image from 'next/image';
-import Link from 'next/link';
+"use client";
+import { ChevronRight, Home, Minus } from "lucide-react";
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const SideBar = () => {
   const navlinks = [
-    { 
-      title: 'Dashboard', icon: <Home />, children: [
-        { title: 'Sales', path: '/', icon: <Minus /> },
-        { title: 'Analytics', path: '/', icon: <Minus /> },
-        { title: 'Finance', path: '/', icon: <Minus /> },
-        { title: 'Crypto', path: '/', icon: <Minus /> }
-      ] 
+    {
+      title: "Dashboard",
+      icon: <Home />,
+      children: [
+        { title: "Sales", path: "/", icon: <Minus /> },
+        { title: "Analytics", path: "/", icon: <Minus /> },
+        { title: "Finance", path: "/", icon: <Minus /> },
+        { title: "Crypto", path: "/", icon: <Minus /> },
+      ],
     },
-    { 
-      title: 'Dashboard', icon: <Home />, children: [
-        { title: 'Sales', path: '/', icon: <Minus /> },
-        { title: 'Analytics', path: '/', icon: <Minus /> },
-        { title: 'Finance', path: '/', icon: <Minus /> },
-        { title: 'Crypto', path: '/', icon: <Minus /> }
-      ] 
+    {
+      title: "Dashboard",
+      icon: <Home />,
+      children: [
+        { title: "Sales", path: "/", icon: <Minus /> },
+        { title: "Analytics", path: "/", icon: <Minus /> },
+        { title: "Finance", path: "/", icon: <Minus /> },
+        { title: "Crypto", path: "/", icon: <Minus /> },
+      ],
     },
-    { 
-      title: 'Dashboard', icon: <Home />, children: [
-        { title: 'Sales', path: '/', icon: <Minus /> },
-        { title: 'Analytics', path: '/', icon: <Minus /> },
-        { title: 'Finance', path: '/', icon: <Minus /> },
-        { title: 'Crypto', path: '/', icon: <Minus /> }
-      ] 
+    {
+      title: "Dashboard",
+      icon: <Home />,
+      children: [
+        { title: "Sales", path: "/", icon: <Minus /> },
+        { title: "Analytics", path: "/", icon: <Minus /> },
+        { title: "Finance", path: "/", icon: <Minus /> },
+        { title: "Crypto", path: "/", icon: <Minus /> },
+      ],
     },
-    { 
-      title: 'Dashboard', icon: <Home />, path: '/' 
+    {
+      title: "Dashboard",
+      icon: <Home />,
+      path: "/",
     },
   ];
 
   return (
     <>
-    <div className='bg-[#0E1726] min-w-[263px] py-1 px-2'>
-      <div>
-        <Image src={'/logo w.png'} width={150} height={200} />
-      </div>
-
-      <div>
-        {
-          navlinks.map((v, i) => {
-            if (v.children) {
-              return (
-                <span>hey</span>
-              )
-            } else {
-
-            }
-          })
-        }
-      </div>
-    </div>
+      {/* <h1>hey</h1> */}
     </>
-  )
-}
+  );
+};
 
-export default SideBar
+export default SideBar;
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,12 +1,82 @@
-import Image from 'next/image'
-import React from 'react'
+'use client'
+import React , {useState  }from "react";
+import Image from "next/image";
+import { Bell, Flag, Mail, Menu, Moon, Search} from 'lucide-react';
+import { Calendar } from 'lucide-react';
+import { SquarePen } from 'lucide-react';
+import { MessageCircleMore } from 'lucide-react';
+import Link from "next/link";
+import { Input } from "@/components/ui/input";
+// import {
+//   Form,
+//   FormControl,
+//   FormField,
+//   FormItem,
+//   FormLabel,
+//   FormMessage,
+// } from "@/components/ui/form";
+
 
 const TopBar = () => {
+  const [isFocused, setIsFocused] = useState(false);
+  
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
   return (
-    <div className='w-full flex bg-[#0E1726] py-2.5 z-20 sticky px-5 text-white'>
-      <Image src={}/>
-    </div>
-  )
-}
+    <>
+      <div className="w-full flex bg-[#0E1726] z-20 sticky px-4 text-white items-center">
+        <Image src={'/logo w.png'} alt='logo' width={150} height={200}/>
+        <button className="bg-[#292F46] rounded-full size-9 mx-1 my-2 hover:bg-[3b82f680] hover:text-[#3B51B9]">
+          <div className=" size-9 rounded-full flex items-center justify-center">
+          <Menu size={20} strokeWidth={1} />
+          </div>
+        </button>
+        <Link href='/' className="bg-[#292F46] rounded-full size-9 mx-1 my-2 hover:bg-[3b82f680] hover:text-[#3B51B9]">
+          <div className=" size-9 rounded-full flex items-center justify-center">
+          <Calendar size={20} strokeWidth={1}/>
+          </div>
+        </Link>
+        <Link href='/' className="bg-[#292F46] rounded-full size-9 mx-1 my-2 hover:bg-[3b82f680] hover:text-[#3B51B9]">
+          <div className=" size-9 rounded-full flex items-center justify-center">
+          <SquarePen size={20} strokeWidth={1} />
+          </div>
+        </Link>
+        <Link href='/' className="bg-[#292F46] rounded-full size-9 mx-1 my-2 hover:bg-[3b82f680] hover:text-[#3B51B9]">
+          <div className=" size-9 rounded-full flex items-center justify-center">
+          <MessageCircleMore size={20} strokeWidth={1} />
+          </div>
+        </Link>
+        <div className='flex-1 relative'>
+          <Search size={20} strokeWidth={0.5} color={isFocused?'#3B51B9':'gray'} className="absolute z-30 top-1/2 left-4 -translate-x-1/2 -translate-y-1/2" />
+          <Input onFocus={() => setIsFocused(true)} onBlur={() => setIsFocused(false)} type='search' placeholder='Search...' className={`w-max pl-8 py-4 bg-[#121E32] text-[#6B7280] rounded-[10px] outline-none border ${ isFocused ? 'border-[#3B51B9]' : 'border-none' }`} />
+        </div>
+        <Link href='/' className="bg-[#292F46] rounded-full size-9 mx-1 my-2 hover:bg-[3b82f680] hover:text-[#3B51B9]">
+          <div className=" size-9 rounded-full flex items-center justify-center">
+          <Moon size={20} strokeWidth={1} />
+          </div>
+        </Link>
+        <button className="bg-[#292F46] rounded-full size-9 mx-1 my-2 hover:bg-[3b82f680] hover:text-[#3B51B9]">
+          <div className=" size-9 rounded-full flex items-center justify-center">
+            <Flag size={20} strokeWidth={1} />
+          </div>
+        </button>
+        <button className="bg-[#292F46] rounded-full size-9 mx-1 my-2 hover:bg-[3b82f680] hover:text-[#3B51B9]">
+          <div className=" size-9 rounded-full flex items-center justify-center">
+            <Mail size={20} strokeWidth={1} />
+          </div>
+        </button>
+        <button className="bg-[#292F46] rounded-full size-9 mx-1 my-2 hover:bg-[3b82f680] hover:text-[#3B51B9]">
+          <div className=" size-9 rounded-full flex items-center justify-center">
+            <Bell size={20} strokeWidth={1} />
+          </div>
+        </button>
+        <div className="bg-[#292F46] rounded-full size-10 mx-1 my-2 cursor-pointer grayscale hover:grayscale-0">
+          <Image src={'/team-1.jpg'} alt="profile" width={50} height={50} className="rounded-full"/>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default TopBar
+export default TopBar;
