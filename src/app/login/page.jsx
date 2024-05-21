271c8b762b8f194a953bdf65e074465d45b94234
ayeshairfan12 <irfanayesha712@gmail.com>
2024-05-21 17:36:13 +0500

    login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -9,12 +9,12 @@ const Page = () => {
     
       <div className="relative z-50 flex flex-col gap-5 items-center justify-center min-h-screen">
         <div>
-          <h1 className=' text-white text-3xl font-bold {poppins.className}'>CRM</h1>
+          <h1 className=' text-white text-3xl font-bold {poppins.classname}'>Customer Relationship Management</h1>
         </div>
-        <div className="bg-[#191947] w-[300px] h-[180px]  rounded-xl shadow-2xl flex items-center justify-center">
+        <div className="bg-[#27407a6c] backdrop-blur-md w-[300px] h-[180px]  rounded-xl shadow-2xl flex items-center justify-center">
           <Loginform />
         </div>
-        <div className=' text-[10px] text-slate-400 underline'>FORGOT YOUR PASSWORD?</div>
+        <div className=' text-[10px] text-slate-200 underline'>FORGOT YOUR PASSWORD?</div>
       </div>
     </div>
   );
