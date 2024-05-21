7cbc417764f0af0b4529b94fc8f0d6e9f610858f
ayeshairfan12 <irfanayesha712@gmail.com>
2024-05-21 17:09:09 +0500

    login

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,23 @@
-import { Inter } from "next/font/google";
+
+import { Poppins } from "next/font/google";
 import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-poppins',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+});
+
 
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html >
       <body>
         {children}</body>
     </html>
   );
 }
+
+
--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -18,17 +18,19 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        login: "bg-white text-black hover:bg-white/50 m-auto "
       },
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
+        login:"rounded-[25px] w-[100px] h-8 px-4 py-2"
       },
     },
     defaultVariants: {
-      variant: "default",
-      size: "default",
+      variant: "login",
+      size: "login",
     },
   }
   
