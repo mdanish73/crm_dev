316bf77b9c2a9059a03c27e32cf0dd909e59c451
OsafTheDeveloper <osafh486@gmail.com>
2024-05-23 19:05:13 +0500

    Make Context API for Superadmin

--- src/app/(Dashboard)/layout.jsx
+++ src/app/(Dashboard)/layout.jsx
@@ -1,4 +1,4 @@
-import SideBar from "@/components/SideBar";
+import SideBar from "@/components/SideBar/SideBar";
 import TopBar from "@/components/Topbar/TopBar";
 
 export default function RootLayout({ children }) {
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,22 +1,23 @@
-'use client'
+"use client";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import SideBarContext from "@/Context/SideBarContext";
+import { Superadmin, } from "@/Context/superadmin/Superadmin";
 
 const poppins = Poppins({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-poppins',
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export default function RootLayout({ children }) {
   return (
-    <html >
+    <html>
       <body>
-        <SideBarContext> 
-          {children}
-        </SideBarContext>
+        <Superadmin>
+          <SideBarContext>{children}</SideBarContext>
+        </Superadmin>
       </body>
     </html>
   );
