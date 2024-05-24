38be24e2d28c0992ec57397bd9119d1a17866579
ayeshairfan12 <irfanayesha712@gmail.com>
2024-05-24 17:44:14 +0500

    profile

--- src/components/Profile/data.jsx
+++ src/components/Profile/data.jsx
@@ -1,25 +1,30 @@
-import React from 'react'
-import employeeModel from '@/backend/models/employees/employee'
+"use client"
+import { SuperadminContext } from '@/Context/superadmin/Superadmin';
+import React, { useContext } from 'react';
 
+const Data = () => {
+  const { data } = useContext(SuperadminContext);
+  console.log(data)
 
-const data = () => {
   return (
     <div>
-       <div className="mt-8 grid grid-cols-1 md:grid-cols-2 gap-6 p-4">
-        {employees.map((employee) => (
-          <div key={employee._id} className="bg-gray-800 p-4 rounded-md">
-            <h2 className="text-xl text-white">{employee.firstName} {employee.lastName}</h2>
-            <p className="text-gray-400">{employee.email}</p>
-            <p className="text-gray-400">{employee.phoneNumber}</p>
-            <p className="text-gray-400">{employee.jobTitle}</p>
-            <p className="text-gray-400">{employee.hireDate}</p>
-            <p className="text-gray-400">{employee.dob}</p>
-            <p className="text-gray-400">{employee.address.house}, {employee.address.street}, {employee.address.area}, {employee.address.postalCode}</p>
-          </div>
-        ))}
-      </div>
+     
+     <div className="mt-8 p-4 bg-gray-800 rounded-md">
+      <h2 className="text-xl text-white">{data.fullname}</h2>
+      <p className="text-gray-400">{data.username}</p>
+      <p className="text-gray-400">{data.password}</p>
+      <p className="text-gray-400">{data.email}</p>
+      <p className="text-gray-400">{data.phonenumber}</p>
+      <p className="text-gray-400">{data.AdminType}</p>
+      <p className="text-gray-400">{data.dob}</p>
+      <p className="text-gray-400">{data.cnic}</p>
+      <p className="text-gray-400">{data.accesslevel}</p>
+     
     </div>
-  )
-}
+    </div>
+  );
+};
 
-export default data
+export default Data;
+
+
