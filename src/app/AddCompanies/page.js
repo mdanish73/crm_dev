aad2791cfd5b292847a81bbf1c46a578e7e3c3ec
codewithmati <m4mati10@gmail.com>
2024-05-25 18:38:00 +0500

    for osaf

---
+++ src/app/AddCompanies/page.js
@@ -0,0 +1,12 @@
+import CompanyForms from '@/components/Forms/CompanyForms'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      <CompanyForms/>
+    </>
+  )
+}
+
+export default page
