767a711e5b7acb3fa163719317cac540b83a2e9d
mdanish73 <mdiqbal00037@gmail.com>
2024-05-27 16:23:45 +0500

    links logic fixed so the routes can be dynamic

--- src/components/SideBar/Links.js
+++ src/components/SideBar/Links.js
@@ -4,7 +4,7 @@ import { Building2, Home, LogOut, NotebookText, Settings, UserCog, Users } from 
 
 const Links = () => {
   const pathname = usePathname();
-
+  console.log("Current Pathname:", pathname);
   const superAdmin = [
     {
       title: 'Dashboard', icon: <Home size={18} />, children: [
@@ -175,12 +175,14 @@ const Links = () => {
     },
   ];
 
-  if (pathname.startsWith === '/dashboard') {
+  if (pathname && pathname.startsWith('/dashboard')) {
     return superAdmin;
-  } else if (pathname.startsWith === '/companies') {
+  } else if (pathname && pathname.startsWith('/companies')) {
     return companyAdmin;
-  } else if (pathname.startsWith === '/branches') {
+  } else if (pathname && pathname.startsWith('/branches')) {
     return branchAdmin;
+  } else {
+    return null;
   }
 }
 
