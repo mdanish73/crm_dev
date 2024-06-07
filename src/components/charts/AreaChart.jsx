496726aa5d39a2ce8c762ae27e15cfa39d80d86e
Muhammad Danish Iqbal <138819121+mdanish73@users.noreply.github.com>
2024-06-07 19:12:12 +0500

    charts added to the dashboard

---
+++ src/components/charts/AreaChart.jsx
@@ -0,0 +1,65 @@
+'use client'
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+  scales,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: false,
+    },
+  },
+  scales: {
+    x: {
+      ticks: {
+        display: false
+      }
+    }
+  }
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      fill: true,
+      label: 'Dataset 2',
+      data: [0, 10, 5, 10, 20, 30, 45, 50, 73, 63, 50, 40],
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.3)',
+      tension: 0.4
+    },
+  ],
+};
+
+export default function AreaChart() {
+  return <Line options={options} data={data} />;
+}
