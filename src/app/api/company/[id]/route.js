2e6e98069bdd6d09c8bf89d5b3f4b56e9c607350
mdanish73 <mdiqbal00037@gmail.com>
2024-08-17 22:12:22 +0500

    cleared error

--- src/app/api/company/[id]/route.js
+++ src/app/api/company/[id]/route.js
@@ -1,86 +1,86 @@
-import dbConnection from "@/backend/db/dbconnection";
-import companyModel from "@/backend/models/company";
-import companyceoModel from "@/backend/models/companies/companyceo";
-import { NextResponse } from "next/server";
+// import dbConnection from "@/backend/db/dbconnection";
+// import companyModel from "@/backend/models/company";
+// import companyceoModel from "@/backend/models/companies/companyceo";
+// import { NextResponse } from "next/server";
 
- dbConnection();
-// Get single company
-const GET = async (req, { params }) => {
-  try {
-    const { id } = params;
-    console.log(id);
-    const company = await companyModel.findById(id);
-    return NextResponse.json(
-      {
-        message: "Company Found",
-        success: true,
-        data: company,
-      },
-      { status: 200 }
-    );
-  } catch (error) {
-    return NextResponse.json(
-      {
-        message: "Internal Server Error",
-        success: false,
-      },
-      { status: 500 }
-    );
-  }
-};
-// Delete The Company
-const DELETE = async (req, { params }) => {
-  try {
-    const { id } = params;
-    console.log(id);
-    const updateDate = await companyModel.findByIdAndDelete(id);
-    return NextResponse.json(
-      {
-        message: "Company Deleted",
-        success: true,
-        data: updateDate,
-      },
-      { status: 200 }
-    );
-  } catch (error) {
-    console.log(error, "From DELETE API");
-    return NextResponse.json(
-      {
-        message: "Internal Server Error",
-        success: false,
-      },
-      { status: 500 }
-    );
-  }
-};
+//  dbConnection();
+// // Get single company
+// const GET = async (req, { params }) => {
+//   try {
+//     const { id } = params;
+//     console.log(id);
+//     const company = await companyModel.findById(id);
+//     return NextResponse.json(
+//       {
+//         message: "Company Found",
+//         success: true,
+//         data: company,
+//       },
+//       { status: 200 }
+//     );
+//   } catch (error) {
+//     return NextResponse.json(
+//       {
+//         message: "Internal Server Error",
+//         success: false,
+//       },
+//       { status: 500 }
+//     );
+//   }
+// };
+// // Delete The Company
+// const DELETE = async (req, { params }) => {
+//   try {
+//     const { id } = params;
+//     console.log(id);
+//     const updateDate = await companyModel.findByIdAndDelete(id);
+//     return NextResponse.json(
+//       {
+//         message: "Company Deleted",
+//         success: true,
+//         data: updateDate,
+//       },
+//       { status: 200 }
+//     );
+//   } catch (error) {
+//     console.log(error, "From DELETE API");
+//     return NextResponse.json(
+//       {
+//         message: "Internal Server Error",
+//         success: false,
+//       },
+//       { status: 500 }
+//     );
+//   }
+// };
 
-// Update The Company
-const PUT = async (req, { params }) => {
-  try {
-    const { id } = params;
-    const body = await req.json();
-    const updateDate = await companyModel.findByIdAndUpdate(id, body, {
-      new: true,
-      runValidators: true,
-    });
-    return NextResponse.json(
-      {
-        message: "User Updated",
-        success: true,
-        data: updateDate,
-      },
-      { status: 200 }
-    );
-  } catch (error) {
-    console.log(error, "From PUT API");
-    return NextResponse.json(
-      {
-        message: "Internal Server Error",
-        success: false,
-      },
-      { status: 500 }
-    );
-  }
-};
+// // Update The Company
+// const PUT = async (req, { params }) => {
+//   try {
+//     const { id } = params;
+//     const body = await req.json();
+//     const updateDate = await companyModel.findByIdAndUpdate(id, body, {
+//       new: true,
+//       runValidators: true,
+//     });
+//     return NextResponse.json(
+//       {
+//         message: "User Updated",
+//         success: true,
+//         data: updateDate,
+//       },
+//       { status: 200 }
+//     );
+//   } catch (error) {
+//     console.log(error, "From PUT API");
+//     return NextResponse.json(
+//       {
+//         message: "Internal Server Error",
+//         success: false,
+//       },
+//       { status: 500 }
+//     );
+//   }
+// };
 
-export { GET, PUT, DELETE };
+// export { GET, PUT, DELETE };
--- src/app/api/countries/[id]/route.js
+++ src/app/api/countries/[id]/route.js
@@ -1,66 +1,66 @@
-import dbConnection from "@/backend/db/dbconnection";
-import countries from "@/backend/models/country/countries";
-import { NextResponse } from 'next/server';
+// import dbConnection from "@/backend/db/dbconnection";
+// import countries from "@/backend/models/country/countries";
+// import { NextResponse } from 'next/server';
 
-dbConnection();
+// dbConnection();
 
-export async function GET(req, {params}) {
-  try {
-    const { id } = params;
-    const country = await countries.findById(id);
-    return NextResponse.json({
-      success: true,
-      message: country
-    }, {
-      status: 200
-    });
-  } catch (error) {
-    return NextResponse.json({
-      success: false,
-      message: error.message
-    }, {
-      status: 500
-    });
-  }
-}
+// export async function GET(req, {params}) {
+//   try {
+//     const { id } = params;
+//     const country = await countries.findById(id);
+//     return NextResponse.json({
+//       success: true,
+//       message: country
+//     }, {
+//       status: 200
+//     });
+//   } catch (error) {
+//     return NextResponse.json({
+//       success: false,
+//       message: error.message
+//     }, {
+//       status: 500
+//     });
+//   }
+// }
 
-export async function PUT (req, {params}) {
-  try {
-    const { id } = params;
-    const request = await req.json();
-    const updatedCountry = await countries.findByIdAndUpdate(id, request, { new: true, runValidators: true });
-    return NextResponse.json({
-      success: true,
-      message: updatedCountry
-    }, {
-      status: 200
-    });
-  } catch (error) {
-    return NextResponse.json({
-      success: false,
-      message: error.message
-    }, {
-      status: 500
-    });
-  }
-}
+// export async function PUT (req, {params}) {
+//   try {
+//     const { id } = params;
+//     const request = await req.json();
+//     const updatedCountry = await countries.findByIdAndUpdate(id, request, { new: true, runValidators: true });
+//     return NextResponse.json({
+//       success: true,
+//       message: updatedCountry
+//     }, {
+//       status: 200
+//     });
+//   } catch (error) {
+//     return NextResponse.json({
+//       success: false,
+//       message: error.message
+//     }, {
+//       status: 500
+//     });
+//   }
+// }
 
-export async function DELETE (req, {params}) {
-  try {
-    const { id } = params;
-    const updatedCountry = await countries.findByIdAndDelete(id);
-    return NextResponse.json({
-      success: true,
-      message: "Country deleted successfully..."
-    }, {
-      status: 200
-    });
-  } catch (error) {
-    return NextResponse.json({
-      success: false,
-      message: error.message
-    }, {
-      status: 500
-    });
-  }
-}
+// export async function DELETE (req, {params}) {
+//   try {
+//     const { id } = params;
+//     const updatedCountry = await countries.findByIdAndDelete(id);
+//     return NextResponse.json({
+//       success: true,
+//       message: "Country deleted successfully..."
+//     }, {
+//       status: 200
+//     });
+//   } catch (error) {
+//     return NextResponse.json({
+//       success: false,
+//       message: error.message
+//     }, {
+//       status: 500
+//     });
+//   }
+// }
